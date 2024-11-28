d4472efbca00df889ffb93c9c73fe94f4b5bafe8
Anuj Kothiyal <anujkothiyal01@gmail.com>
2024-11-28 16:34:16 +0530

    q3

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client'; // Client-side rendering for this component
 
+import About from './components/About';
 import Header from './components/Header';
 import './globals.css'; // Import your global CSS
 
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <main className="bg-background text-foreground">
       <Header/>
+      <About/>
     </main>
 
   );
