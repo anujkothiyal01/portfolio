f7eab0b42b98856effa3fc3e330e53da7e6f1ddb
Anuj Kothiyal <anujkothiyal01@gmail.com>
2024-11-28 16:44:25 +0530

    q6

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client'; // Client-side rendering for this component
 
 import About from './components/About';
+import Contact from './components/Contact';
 import Header from './components/Header';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
@@ -13,6 +14,7 @@ export default function Home() {
       <About/>
       <Skills/>
       <Projects/>
+      <Contact/>
     </main>
 
   );
